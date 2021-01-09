5df1836e6e652da0de4f0fff2e8043f253d88fba
Alex Sinelshchikov <alexsinelshchikov2303@gmail.com>
2021-01-09 15:40:47 +0300

    Задание по циклам

---
+++ 2.5/src/Cycle.java
@@ -0,0 +1,12 @@
+public class Cycle {
+
+    public static void main(String[] args) {
+
+
+       for (int tickets = 200000; tickets <= 210000; tickets++ ) {
+           System.out.println(tickets);
+       }
+
+
+    }
+}
---
+++ 2.5/src/While.java
@@ -0,0 +1,10 @@
+public class While {
+
+    public static void main(String[] args) {
+        int tickets = 220000;
+        while (tickets <= 235000) {
+            System.out.println(tickets);
+            tickets++;
+        }
+    }
+}
